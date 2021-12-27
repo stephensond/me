00ccd274fee8735612455f926fc8e41b3872ad65
David Stephenson <stephenson.d@northeastern.edu>
2021-12-27 13:10:21 -0500

    a few updates

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "../styles/index.css";
+import React from 'react';
+import '../styles/index.css';
 
-const HomePage = () => {
+function HomePage() {
   return (
     <div className="background">
       <h1 className="content">Under construction.</h1>
       <p className="subcontent">Coming soon!</p>
     </div>
   );
-};
+}
 
 export default HomePage;
