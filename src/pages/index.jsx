e83cc7b88757847abf8f26c98aa5588caee2eb32
David Stephenson <stephenson.d@northeastern.edu>
2022-01-09 18:20:54 -0500

    add footer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,6 +23,30 @@ function HomePage() {
       <div className={styles.interests}>
         <Interests />
       </div>
+      <div className={styles.footer}>
+        Built with
+        {' '}
+        <a
+          href="https://www.gatsbyjs.com/"
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gatsby
+        </a>
+        {' '}
+        and
+        {' '}
+        <a
+          href="https://reactjs.org/"
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+        .
+      </div>
     </div>
   );
 }
